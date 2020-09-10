9035abfca9a54c4be800ac8f9491655df1499afb
Owolabi Gideon <owolabigidslab@gmail.com>
2020-09-10 01:45:52 +0100

    updated files

--- src/screens/main/PostSettingScreen.js
+++ src/screens/main/PostSettingScreen.js
@@ -1,16 +1,17 @@
 import React, { Component, useEffect, useState } from 'react';
-import { SafeAreaView, StyleSheet, View } from 'react-native';
+import { SafeAreaView, StyleSheet, View, ScrollView } from 'react-native';
 import { useTheme } from '../../assets/themes'
 import { Navigation, } from 'react-native-navigation';
 import { LoaderScreen } from '../../components/reusable/ResuableWidgets';
 import {
     responsiveFontSize, responsiveHeight, responsiveWidth
 } from 'react-native-responsive-dimensions';
-import { Icon, Text } from 'react-native-elements';
+import { Icon, Text, CheckBox, ListItem } from 'react-native-elements';
 import { Header } from '../../components/reusable/ResuableWidgets';
 import { connect } from 'react-redux';
 import * as actions from '../../actions';
 import { checkData } from '../../utilities/index';
+import TouchableScale from 'react-native-touchable-scale/src/TouchableScale';
 
 const { colors } = useTheme();
 
@@ -62,8 +63,6 @@ const PostSettingScreen = ({
             />
             {
                 loaded == false ? <LoaderScreen
-                    animationOff={true}
-                    showLoading={true}
                     loaderIcon={<Icon
                         type="antdesign"
                         name="setting"
@@ -71,7 +70,58 @@ const PostSettingScreen = ({
                         size={responsiveFontSize(10)}
                     />}
                     animationType={'zoomIn'}
-                /> : <></>
+                /> :
+                    <ScrollView showsVerticalScrollIndicator={false}
+                        keyboardShouldPersistTaps='always'
+                        keyboardDismissMode={'on-drag'}
+                    >
+                        <View style={styles.contentContainerStyle}>
+                            <View>
+                                <Text style={{
+                                    color: colors.text,
+                                    fontSize: responsiveFontSize(2.4),
+                                    fontWeight: "bold"
+                                }}>Post Range</Text>
+                                <Text style={{ color: colors.iconcolor }}>
+                                    Restrict posts that appear on your timeline
+                                    </Text>
+                                <CheckBox
+                                    title="All posts"
+                                    //checked={true}
+                                    containerStyle={styles.checkBoxCtn}
+                                />
+                                <CheckBox
+                                    title="Posts from my campus only"
+                                    containerStyle={styles.checkBoxCtn}
+                                />
+                                <CheckBox
+                                    title="Posts from people am following only"
+                                    containerStyle={styles.checkBoxCtn}
+                                />
+                            </View>
+
+                            <View>
+                                <ListItem
+                                    title={'Blacklisted Posts'}
+                                    subtitle={'Press to view posts you blacklisted'}
+                                    subtitleStyle={{ color: colors.iconcolor }}
+                                    Component={TouchableScale}
+                                    activeScale={0.9}
+                                    containerStyle={styles.listItem}
+                                    friction={90}
+                                    //onPress={() => console.warn('coool')}
+                                    titleStyle={{ color: colors.text, fontWeight: "bold" }}
+                                    chevron={{
+                                        name: 'block',
+                                        size: responsiveFontSize(2.6),
+                                        color: colors.text,
+                                        type: 'entypo'
+                                    }}
+                                />
+                            </View>
+
+                        </View>
+                    </ScrollView>
             }
         </SafeAreaView >);
 };
@@ -91,6 +141,23 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: colors.background
     },
+    postRangeContainer: {
+
+    },
+    contentContainerStyle: {
+        flex: 1,
+        marginLeft: 20,
+        marginTop: 10,
+        backgroundColor: colors.background,
+    },
+    listItem: {
+        backgroundColor: colors.background
+    },
+    checkBoxCtn: {
+        backgroundColor: colors.background,
+        borderColor: colors.iconcolor,
+        borderRadius: 10
+    }
 
 });
 
