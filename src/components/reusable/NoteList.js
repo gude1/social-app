1c58d9a929e050b8f3c5149b13a89e071f3fae3e
gude1 <owolabigidslab@gmail.com>
2021-10-18 00:24:54 +0100

    update

--- src/components/reusable/NoteList.js
+++ src/components/reusable/NoteList.js
@@ -11,6 +11,7 @@ import {
 import {Navigation} from 'react-native-navigation';
 import {Text, Button, Icon, ListItem} from 'react-native-elements';
 import {useTheme} from '../../assets/themes/index';
+import {AvatarNavModal, PanelMsg} from './ResuableWidgets';
 //import {ListItem} from './ResuableWidgets';
 
 const {colors} = useTheme();
@@ -103,7 +104,68 @@ const _keyExtractor = (item, index) => index.toString();
 export class NotificationList extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      avatarnavmodal: {
+        visible: false,
+        avatar: null,
+        profile: null,
+        headername: '',
+      },
+    };
+    this.navmodallistitem = [
+      {
+        icon: {
+          name: 'user',
+          type: 'evilicon',
+        },
+        onPress: () => {
+          this.setState({
+            avatarnavmodal: {
+              ...this.state.avatarnavmodal,
+              visible: false,
+            },
+          });
+          Navigation.showModal({
+            component: {
+              name: 'ViewProfile',
+              passProps: {
+                navparent: true,
+                reqprofile: this.state.avatarnavmodal.profile,
+                screentype: 'modal',
+              },
+            },
+          });
+        },
+      },
+      {
+        icon: {
+          name: 'comment',
+          type: 'evilicon',
+        },
+        onPress: () => {
+          this.setState({
+            avatarnavmodal: {
+              ...this.state.avatarnavmodal,
+              visible: false,
+            },
+          });
+
+          Navigation.showModal({
+            component: {
+              name: 'PrivateChat',
+              passProps: {
+                navparent: true,
+                propfindpartnerchat: true,
+                privatechatobj: {
+                  partnerprofile: this.state.avatarnavmodal.profile,
+                },
+                screentype: 'modal',
+              },
+            },
+          });
+        },
+      },
+    ];
   }
 
   componentDidMount() {
@@ -113,6 +175,23 @@ export class NotificationList extends Component {
     }
   }
 
+  _onAvatarPress = profile => {
+    if (isEmpty(profile)) {
+      return;
+    }
+    return () => {
+      this.setState({
+        avatarnavmodal: {
+          ...this.state.avatarnavmodal,
+          headername: profile.user.username,
+          profile: profile,
+          avatar: profile.avatar[1],
+          visible: true,
+        },
+      });
+    };
+  };
+
   _arrangeNote = item => {
     if (isEmpty(item)) {
       return null;
@@ -121,7 +200,7 @@ export class NotificationList extends Component {
     let count_msg = null;
     switch (item.type) {
       case 'postlike':
-        msg = `${item.post.post_text}`;
+        msg = `${item?.post?.post_text}`;
         count_msg =
           item.related_count > 0
             ? `${item.initiator_profile.profile_name} and ${
@@ -130,14 +209,11 @@ export class NotificationList extends Component {
             : `${item.initiator_profile.profile_name} liked your post`;
         return (
           <ListItem
-            // title={}
-            leftAvatar={{source: {uri: item.initiator_profile.avatar[0]}}}
-            containerStyle={{
-              backgroundColor: colors.background,
-              paddingLeft: 30,
-              borderWidth: 0.5,
-              borderColor: colors.border,
+            leftAvatar={{
+              source: {uri: item.initiator_profile.avatar[1]},
+              onPress: this._onAvatarPress(item.initiator_profile),
             }}
+            containerStyle={styles.containerStyle}
             title={count_msg}
             titleStyle={{fontWeight: 'bold', color: colors.text}}
             subtitle={msg}
@@ -146,7 +222,7 @@ export class NotificationList extends Component {
         );
         break;
       case 'postcomment':
-        msg = `${item.comment.comment_text}`;
+        msg = `${item?.comment?.owner_post?.post_text}`;
         count_msg =
           item.related_count > 0
             ? `${item.initiator_profile.profile_name} and ${
@@ -155,16 +231,11 @@ export class NotificationList extends Component {
             : `${item.initiator_profile.profile_name} commented on your post`;
         return (
           <ListItem
-            // title={}
             leftAvatar={{
-              source: {uri: item.initiator_profile.avatar[0]},
+              source: {uri: item.initiator_profile.avatar[1]},
+              onPress: this._onAvatarPress(item.initiator_profile),
             }}
-            containerStyle={{
-              backgroundColor: colors.background,
-              paddingLeft: 30,
-              borderWidth: 0.5,
-              borderColor: colors.border,
-            }}
+            containerStyle={styles.containerStyle}
             title={count_msg}
             titleStyle={{fontWeight: 'bold', color: colors.text}}
             subtitle={msg}
@@ -173,7 +244,7 @@ export class NotificationList extends Component {
         );
         break;
       case 'postcommentlike':
-        msg = `${item.comment.comment_text}`;
+        msg = `${item?.comment?.comment_text}`;
         count_msg =
           item.related_count > 0
             ? `${item.initiator_profile.profile_name} and ${
@@ -182,16 +253,11 @@ export class NotificationList extends Component {
             : `${item.initiator_profile.profile_name} liked your comment`;
         return (
           <ListItem
-            // title={}
             leftAvatar={{
-              source: {uri: item.initiator_profile.avatar[0]},
+              source: {uri: item.initiator_profile.avatar[1]},
+              onPress: this._onAvatarPress(item.initiator_profile),
             }}
-            containerStyle={{
-              backgroundColor: colors.background,
-              paddingLeft: 30,
-              borderWidth: 0.5,
-              borderColor: colors.border,
-            }}
+            containerStyle={styles.containerStyle}
             title={count_msg}
             titleStyle={{fontWeight: 'bold', color: colors.text}}
             subtitle={msg}
@@ -199,27 +265,74 @@ export class NotificationList extends Component {
           />
         );
         break;
-      default:
+      case 'postcommentreply':
+        msg = `${item?.postcommentreply?.origin?.comment_text}`;
+        count_msg =
+          item.related_count > 0
+            ? `${item.initiator_profile.profile_name} and ${
+                item.related_count
+              } others replied to your comment`
+            : `${item.initiator_profile.profile_name} replied to your comment`;
         return (
           <ListItem
-            // title={}
             leftAvatar={{
-              source: {uri: item.initiator_profile.avatar[0]},
+              source: {uri: item.initiator_profile.avatar[1]},
+              onPress: this._onAvatarPress(item.initiator_profile),
             }}
-            containerStyle={{
-              backgroundColor: colors.background,
-              paddingLeft: 30,
-              borderWidth: 0.5,
-              borderColor: colors.border,
+            containerStyle={styles.containerStyle}
+            title={count_msg}
+            titleStyle={{
+              fontWeight: 'bold',
+              color: colors.text,
             }}
-            title={`${item.initiator_profile.profile_name} and ${
-              item.related_count
-            } others like your comment`}
+            subtitle={msg}
+            subtitleStyle={{
+              fontFamily: 'serif',
+              color: colors.placeholder,
+              textAlign: 'justify',
+            }}
+          />
+        );
+        break;
+      case 'postcommentreplylike':
+        msg = `${item?.postcommentreply?.reply_text}`;
+        isEmpty(item.postcommentreply) && alert(JSON.stringify(item));
+        count_msg =
+          item.related_count > 0
+            ? `${item.initiator_profile.profile_name} and ${
+                item.related_count
+              } others liked your reply`
+            : `${item.initiator_profile.profile_name} liked your reply`;
+        return (
+          <ListItem
+            leftAvatar={{
+              source: {uri: item.initiator_profile.avatar[1]},
+              onPress: this._onAvatarPress(item.initiator_profile),
+            }}
+            containerStyle={styles.containerStyle}
+            title={count_msg}
             titleStyle={{fontWeight: 'bold', color: colors.text}}
-            subtitle={`i like everything beautiful and perfectly made inluding you`}
+            subtitle={msg}
             subtitleStyle={{color: colors.placeholder}}
           />
         );
+        break;
+      case 'profilefollow':
+        msg = `${item.initiator_profile.profile_name} started following you`;
+        return (
+          <ListItem
+            leftAvatar={{
+              source: {uri: item.initiator_profile.avatar[1]},
+              onPress: this._onAvatarPress(item.initiator_profile),
+            }}
+            containerStyle={styles.containerStyle}
+            title={msg}
+            titleStyle={{fontWeight: 'bold', color: colors.text}}
+          />
+        );
+        break;
+      default:
+        return <PanelMsg message={item.type} />;
         break;
     }
   };
@@ -239,17 +352,52 @@ export class NotificationList extends Component {
     let {list, loading, loadingmore, fetchNotes} = this.props;
     list = list || [];
     return (
-      <FlatList
-        data={list}
-        keyExtractor={_keyExtractor}
-        initialNumRender={5}
-        showsVerticalScrollIndicator={false}
-        renderItem={this.renderItem}
-        ListEmptyComponent={_setEmptyPlaceHolder(loading, fetchNotes)}
-        ListFooterComponent={_setFooterComponent(loadingmore, list.length, () =>
-          fetchNotes([true]),
-        )}
-      />
+      <>
+        <FlatList
+          data={list}
+          keyExtractor={_keyExtractor}
+          initialNumRender={5}
+          showsVerticalScrollIndicator={false}
+          renderItem={this.renderItem}
+          ListEmptyComponent={_setEmptyPlaceHolder(loading, fetchNotes)}
+          ListFooterComponent={_setFooterComponent(
+            loadingmore,
+            list.length,
+            () => fetchNotes([true]),
+          )}
+        />
+        <AvatarNavModal
+          avatar={this.state.avatarnavmodal.avatar}
+          isVisible={this.state.avatarnavmodal.visible}
+          onBackdropPress={() =>
+            this.setState({
+              avatarnavmodal: {
+                ...this.state.avatarnavmodal,
+                visible: false,
+              },
+            })
+          }
+          onAvatarPress={() => {
+            this.setState({
+              avatarnavmodal: {
+                ...this.state.avatarnavmodal,
+                visible: false,
+              },
+            });
+            Navigation.showModal({
+              component: {
+                name: 'PhotoViewer',
+                passProps: {
+                  navparent: true,
+                  photos: [this.state.avatarnavmodal.avatar],
+                },
+              },
+            });
+          }}
+          headername={this.state.avatarnavmodal.headername}
+          navBarItemArr={this.navmodallistitem}
+        />
+      </>
     );
   }
 }
@@ -287,4 +435,12 @@ export class MentionsList extends Component {
   }
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  containerStyle: {
+    backgroundColor: colors.background,
+    marginBottom: 1,
+    paddingLeft: 30,
+    borderBottomWidth: 0.3,
+    borderColor: colors.border,
+  },
+});
